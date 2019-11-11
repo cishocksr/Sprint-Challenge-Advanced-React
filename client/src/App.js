5a785bdb20c31b2d95f96c0dc10de8a30151e7fb
cishocksr <cishockleysr@gmail.com>
2019-11-10 21:51:20 -0500

    folders setup/add axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(response => {
+        console.log(response);
+        this.setState({
+          players: response.data
+        });
+      })
+      .catch(error => {
+        console.log("axios: ", error);
+      });
+  }
+
+  render() {
+    return <div>Rendering...</div>;
+  }
 }
 
 export default App;
